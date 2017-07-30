c9902627d340d1a04efa2d3e47dc1ad47e08322d
unknown <670609976@qq.com>
2017-07-30 22:46:20 +0800

    first commit

---
+++ cloud-simple-service/src/main/java/cloud/simple/server/system/controller/UserController.java
@@ -0,0 +1,23 @@
+package cloud.simple.server.system.controller;
+
+import cloud.simple.server.system.domain.UserService;
+import cloud.simple.server.system.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value="/user",method=RequestMethod.GET)
+	public List<User> readUserInfo(){
+		List<User> ls=userService.searchAll();		
+		return ls;
+	}
+}
